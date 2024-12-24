500f2b23a1cff59668dc805f0f29a110b98e1012
Mona Lisa <yibifur2002@gmail.com>
2024-12-24 23:39:38 +0300

    first

---
+++ PetCareSystem/src/main/java/com/example/PetCareSystem/PetCareSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.PetCareSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetCareSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetCareSystemApplication.class, args);
+	}
+
+}
---
+++ PetCareSystem/src/test/java/com/example/PetCareSystem/PetCareSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PetCareSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetCareSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
