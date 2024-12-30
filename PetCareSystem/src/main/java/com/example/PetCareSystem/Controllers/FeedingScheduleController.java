6051ef88f4501a9db7c020b97eb9dd4838b1d3ba
YagizSahin <yagizsahin141@outlook.com>
2024-12-30 04:29:00 +0300

    endpoint düzenlendi

--- PetCareSystem/src/main/java/com/example/PetCareSystem/Controllers/FeedingScheduleController.java
+++ PetCareSystem/src/main/java/com/example/PetCareSystem/Controllers/FeedingScheduleController.java
@@ -5,22 +5,23 @@ import com.example.PetCareSystem.Entities.FeedingSchedule;
 import com.example.PetCareSystem.Services.FeedingScheduleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/pets/{petId}/feeding-schedule")
+@RequestMapping("/feeding-schedule")
 public class FeedingScheduleController {
 
     @Autowired
     private FeedingScheduleService feedingScheduleService;
-
-    @PostMapping
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/pets/{petId}/add")
     public ResponseEntity<FeedingScheduleDTO> addFeedingSchedule(@PathVariable int petId, @RequestBody FeedingSchedule feedingSchedule) {
         FeedingScheduleDTO savedSchedule = feedingScheduleService.addFeedingSchedule(petId, feedingSchedule);
         return ResponseEntity.ok(savedSchedule);
     }
-
-    @GetMapping
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/pets/{petId}/get")
     public ResponseEntity<FeedingScheduleDTO> getFeedingSchedule(@PathVariable int petId) {
         FeedingScheduleDTO feedingSchedule = feedingScheduleService.getFeedingScheduleByPetId(petId);
         return ResponseEntity.ok(feedingSchedule);
